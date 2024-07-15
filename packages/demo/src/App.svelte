ba0ddc36ae6a67f3cbf17ca8b7809857b9e88351
ivanzavisic <ivan.zavisic@gmail.com>
2024-07-15 17:44:14 +0200

    chore: update demo page

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -213,9 +213,9 @@
       <div class="flex flex-col">
         <div style="width: 630px;height: 49px; border: 1px solid red;"><ga-dyslipidemia subtype="mature" report="1K4GyKDgsB9ATQflwio7"></ga-dyslipidemia></div>
         <div style="height: 20px;"></div>
-        <div style="width: 630px;height: 49px; border: 1px solid red;"><ga-dyslipidemia subtype="median" report="1K4GyKDgsB9ATQflwio7"></ga-dyslipidemia></div>
-        <div style="height: 20px;"></div>
         <div style="width: 630px;height: 49px; border: 1px solid red;"><ga-dyslipidemia subtype="youth" report="1K4GyKDgsB9ATQflwio7"></ga-dyslipidemia></div>
+        <div style="height: 20px;"></div>
+        <div style="width: 630px;height: 49px; border: 1px solid red;"><ga-dyslipidemia subtype="shield" report="1K4GyKDgsB9ATQflwio7"></ga-dyslipidemia></div>
         <div style="height: 20px;"></div>
         <div style="width: 630px;height: 49px; border: 1px solid red;"><ga-dyslipidemia subtype="lifestyle" report="1K4GyKDgsB9ATQflwio7"></ga-dyslipidemia></div>
         <div style="height: 20px;"></div>
