bbd6b3121b90a056d09dac58b2027a66f917c425
bdeana <deana.bubanj@gmail.com>
2024-10-10 13:48:56 +0200

    commit3

--- packages/lib/src/scatter-plot/scatter-plot.wc.svelte
+++ packages/lib/src/scatter-plot/scatter-plot.wc.svelte
@@ -44,12 +44,12 @@
     let score = 0;
     let show = false;
     let chart: any = {};
-    let gender = ''  ;
+    let gender = '';
     let glycanAge = 0;
 
     const details = types.find(x => x.name === type);
 
-    function ChooseColor(){
+    function getColor(){
         if (type == 'median') {
             return getColorMedianWithPercentile(percentile);
         }
@@ -59,7 +59,6 @@
         if (type == 'mature' || type == 'lifestyle'){
             return getColorBlueToRedWithPercentile(percentile);
         }
-
     }
 
     function drawGraph() {
@@ -67,20 +66,10 @@
         const chartElement = document.getElementById('chart') as HTMLElement;
         if (chartElement) {
             chart = echarts.init(chartElement);
-
         } else {
             console.error("Chart element not found.");
         }
 
-        
-        //var wanted_score = 45;
-        //var selectedIndex = scatterData.data.findIndex(subarray => subarray.x === wanted_age);
-        
-        
-        var age = glycanAge
-        var indexScore = score
-
-        
         echarts.registerTransform(ecStat['transform'].regression);
         
         var option = {
@@ -88,34 +77,33 @@
             backgroundColor: 'transparent',
             dataset: [{
                 source: scatterData.data.map(item => [item.x, item.y])
-            }, {
+            }, 
+            {
                 transform: {
                     type: 'ecStat:regression',
                     config: { method: 'polynomial', order: 3 }
                 }
             }],
             tooltip: {
-                show: false // Hide tooltips for all points
+                show: false 
             },
             xAxis: {
-                min: 0,           // Minimum value of the X-axis
-                max: 100,         // Maximum value of the X-axis
+                min: 0,
+                max: 100,
                 interval: 20,
                 name: 'Age',
                 axisLine: {
-                    show: true, // Show the X axis line
-                    symbol: ['none', 'arrow'], // Add arrow at the end
-                    symbolSize: [5, 10], // Size of the arrows
-                    
+                    show: true, 
+                    symbol: ['none', 'arrow'],
+                    symbolSize: [5, 10]
                 },
                 axisTick: {
-                    show: false // Hide the X axis ticks
+                    show: false
                 },
                 axisLabel: {
-                    show: true, // Show the X axis labels
-                    color: 'black', // Optional: Customize label color
+                    show: true,
+                    color: 'black',
                     formatter: function(value) {
-                        // Only show labels for 20, 40, 60, and 80
                         return value === 0 || value === 100 ? '' : value;
                     }
                 },
@@ -128,49 +116,44 @@
                 max:0.6,
                 name: 'Index score',
                 axisLine: {
-                    show: true, // Show the Y axis line
-                    symbol: ['none', 'arrow'], // Add arrow at the end
-                    symbolSize: [5, 10], // Size of the arrows
+                    show: true,
+                    symbol: ['none', 'arrow'],
+                    symbolSize: [5, 10],
                 },
                 axisTick: {
-                    show: false // Hide the Y axis ticks
+                    show: false
                 },
                 axisLabel: {
-                    show: true, // Show the Y axis labels
-                    color: 'black', // Optional: Customize label color
+                    show: true,
+                    color: 'black',
                     formatter: function(value) {
-                        // Only show labels for 20, 40, 60, and 80
                         return value === 0|| value === 0.6 ? '' : value;
                     }
                 },
                 splitLine: {
-                    
                     show: true
                 }
             },
             series: [{
-                //name: 'scatter', // Scatter series for dots, appears first
                 type: 'scatter',
-                tooltip: {}, // Enable tooltip for scatter points if needed
+                tooltip: {},
                 itemStyle: {
                     color: '#D3D3D3',
                     opacity: 0.5,
-                    borderWidth: 1,
-                    
+                    borderWidth: 1
                 },
-                showInLegend: false // 
-                
+                showInLegend: false
             },
             {
-                    name: 'Measured result',
-                    type: 'scatter',
-                    data: [{ x: age, y: indexScore }], 
-                    symbolSize: 10, 
-                    itemStyle: {
-                        color: ChooseColor(), 
-                    },
-                    showSymbol:true
+                name: 'Measured result',
+                type: 'scatter',
+                data: [{ x: glycanAge, y: score }], 
+                symbolSize: 10, 
+                itemStyle: {
+                    color: getColor(), 
                 },
+                showSymbol:true
+            },
             {
                 name: 'Population average',
                 type: 'line',
@@ -187,11 +170,8 @@
                     width: 5, 
                     opacity: 0.7,
             },
-            
             showSymbol: false,
-            },
-            
-        ],
+            }],
             graphic: [],
             legend: {
                 show: true,
@@ -201,27 +181,26 @@
                 textStyle: {
                     color: 'black'
                 },
-            selected: {
-                'Measured result': true,
-                'Population average': true,
-                },
+                selected: {
+                    'Measured result': true,
+                    'Population average': true,
+                    },
                 inactiveColor: '#fff',
             }
         };
-
 
         chart.setOption(option);
                 
         option.graphic.push({
             type: 'line',
             shape: {
-            x1: chart.convertToPixel('xAxis', age),
+            x1: chart.convertToPixel('xAxis', glycanAge),
             y1: chart.convertToPixel('yAxis', 0),
-            x2: chart.convertToPixel('xAxis', age),
-            y2: chart.convertToPixel('yAxis', indexScore)
+            x2: chart.convertToPixel('xAxis', glycanAge),
+            y2: chart.convertToPixel('yAxis', score)
             },
             style: {
-            stroke: ChooseColor(),
+            stroke: getColor(),
             lineDash: [8, 8],
             lineWidth: 3
             },
@@ -232,51 +211,49 @@
             type: 'line',
             shape: {
             x1: chart.convertToPixel('xAxis', 0),
-            y1: chart.convertToPixel('yAxis', indexScore),
-            x2: chart.convertToPixel('xAxis', age),
-            y2: chart.convertToPixel('yAxis', indexScore)
+            y1: chart.convertToPixel('yAxis', score),
+            x2: chart.convertToPixel('xAxis', glycanAge),
+            y2: chart.convertToPixel('yAxis', score)
             },
             style: {
-            stroke: ChooseColor(),
+            stroke: getColor(),
             lineDash: [8, 8],
             lineWidth: 3
             },
             z: 8
         });
+
         option.graphic.push({
             type: 'text',
             style: {
-            x: chart.convertToPixel('xAxis', age) + 25, 
-            y: chart.convertToPixel('yAxis', indexScore) - 10, 
-            text: `${Number.parseFloat(indexScore).toFixed(5)}`, 
-            fill: 'white', // Text color
+            x: chart.convertToPixel('xAxis', glycanAge) + 25, 
+            y: chart.convertToPixel('yAxis', score) - 10, 
+            text: `${Number.parseFloat(score).toFixed(5)}`, 
+            fill: 'white', 
             font: 'bold 17px sans-serif',
-            //width:'5px',
-            borderColor: ChooseColor(),
+            borderColor: getColor(),
             borderWidth: 10,
             borderRadius: 2,
-            backgroundColor: ChooseColor(),
+            backgroundColor: getColor(),
             },
             z: 10 
         });
-        const largerDotSize = 7; // Adjust the size as needed
+
         option.graphic.push({
             name: 'Measured result',
             type: 'circle',
             shape: {
-                cx: chart.convertToPixel('xAxis', age),
-                cy: chart.convertToPixel('yAxis', indexScore),
-                r: largerDotSize // Set the radius of the larger dot
+                cx: chart.convertToPixel('xAxis', glycanAge),
+                cy: chart.convertToPixel('yAxis', score),
+                r: 7
             },
             style: {
-                fill: ChooseColor(), // Color of the larger dot
-                //stroke: 'white',
-                //lineWidth: 2
+                fill: getColor(), 
             },
-            z: 15 // Ensure it is drawn on top of everything
+            z: 15
         });
 
-    chart.setOption(option); // Set the final options to the chart
+    chart.setOption(option); 
 }
 
     onMount(async () => {
@@ -284,17 +261,9 @@
         gender= reportData.sex;
         scatterData = await service.getScatterData(type, gender);
         glycanAge = Number(reportData.glycanage);
-        
-        console.log('reportData:', reportData);
-        console.log('scatterData', scatterData['data']);
-        
-        // Ensure the correct data is assigned to percentile and score
+
         if (details) {
-            percentile = Number(reportData[details.csvPerc]);
-            console.log(type);
-            
-            console.log('perc:', percentile);
-            
+            percentile = Number(reportData[details.csvPerc]);            
             score = Number(reportData[details.csvScore]);
         }
 
